d849ddd2a6dbd9f978d4699852eb68115e5f3b6c
pervakam <52745442+pervakam@users.noreply.github.com>
2020-07-17 11:16:21 +0300

    завершает окно заявка принята

--- source/js/main.js
+++ source/js/main.js
@@ -8,6 +8,45 @@
   var callbackModal = document.querySelector('.callback-modal');
   var callbackCloseButton = document.querySelector('.callback-modal__close');
   var callbackOpenButton = document.querySelector('.page-header__contact-callback');
+  var callbackForm =document.querySelector('.callback-modal__form');
+  var nameField = callbackForm.querySelector('input[name="name-field"]');
+
+  var successModalHide = document.querySelector('.success-modal-hide');
+  var successModal = document.querySelector('.success-modal');
+  var successCloseButton = document.querySelector('.success-modal__close');
+  var successOkButton = document.querySelector('.success-modal__ok');
+
+  var closeSuccessModal = function () {
+    successModal.classList.add('success-modal-hide');
+  };
+
+  var closeSuccessModalByClick = function () {
+    successCloseButton.addEventListener('click', closeSuccessModal);
+    successOkButton.addEventListener('click', closeSuccessModal);
+
+    window.addEventListener('keydown', function (evt) {
+      if (evt.key === ESC_KEY) {
+        evt.preventDefault();
+        closeSuccessModal();
+      }
+    });
+
+    document.addEventListener('click', function (evt) {
+      if (evt.target !== callbackOpenButton && evt.target !== successModal && !successModal.contains(evt.target)) {
+        closeSuccessModal()
+      }
+    });
+  };
+
+  closeSuccessModalByClick();
+
+
+
+
+
+
+
+
 
   var closeCallbackModal = function () {
     callbackModal.classList.add('callback-modal-hide');
@@ -32,6 +71,8 @@
 
   var openCallbackModal = function () {
     callbackModal.classList.remove('callback-modal-hide');
+    nameField.focus();
+
     closeCallbackModalByClick();
   };
 
