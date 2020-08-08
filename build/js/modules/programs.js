6aa41e520a595850ed57919667e3dae1ea947720
pervakam <52745442+pervakam@users.noreply.github.com>
2020-08-08 12:15:04 +0300

    корректировка по замечаниям наставника

--- build/js/modules/programs.js
+++ build/js/modules/programs.js
@@ -16,39 +16,4 @@
     });
   });
 
-  var swiperActivation = function () {
-    programSwiperContainer.classList.add('swiper-container');
-    programSwiperWrapper.classList.add('swiper-wrapper');
-    programTypeButton.forEach(function (it) {
-      it.classList.add('swiper-slide');
-    });
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 'auto',
-    });
-    programSwiperWrapper.style.width = PROGRAM_NAME_MOB_WIDTH;
-  };
-
-  var swiperDeactivation = function () {
-    programSwiperContainer.classList.remove('swiper-container');
-    programSwiperWrapper.classList.remove('swiper-wrapper');
-    programTypeButton.forEach(function (it) {
-      it.classList.remove('swiper-slide');
-    })
-    programSwiperWrapper.style.width = PROGRAM_NAME_DESK_WIDTH;
-  };
-
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    swiperActivation()
-  }
-
-  var programDisplaySizeHandler = function () {
-    if (window.innerWidth <= 767) {
-      swiperActivation()
-    } else if (window.innerWidth >= 768) {
-      swiperDeactivation()
-    }
-  };
-
-  window.addEventListener('resize', programDisplaySizeHandler);
-
 })();
